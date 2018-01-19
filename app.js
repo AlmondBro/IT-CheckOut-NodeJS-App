ea7fdf91623629850e6180436ab7392f7df65178
Juan David Lopez Londoño <juandavidlopez95@yahoo.com>
2018-01-19 12:03:58 -0800

    Updated NPM script engines specification. The Node version that is used on the local machine is specified.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ console.log('Your username', ':', currentUserName);
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-const mongoDB = "mongodb://JuanDa95:yolo95@ds155091.mlab.com:55091/cv-it-checkout";
+const mongoDB = MONGODB_LAB_URI;
 //mongoose.connect(mongoDB);
 mongoose.createConnection(mongoDB);
 var db = mongoose.connection;
